0ebc8dd8a65dffdd1adc3f8d48fd7d5b5150863e
Melvin Majano <melvinmajano16@gmail.com>
2026-08-19 17:53:28 -0600

    Se creo la logica del checkToken

---
+++ api/src/Modules/Core/Services/AuthService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Core;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Infrastructure\Base\BaseService;
+use Throwable;
+
+class AuthService extends BaseService
+{
+    public function checkToken(string $token):?object
+    {
+       try{
+        $secrect = $_ENV['JWT_SECRET']?? 'default_secret';
+        return JWT::decode($token, new Key($secrect,'HS256'));
+       }catch(Throwable $e){
+         return null;
+       }
+    }
+}
