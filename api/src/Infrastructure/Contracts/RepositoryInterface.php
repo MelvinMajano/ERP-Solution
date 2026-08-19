af2f3994502615788ffbc8eb0eca87a0995e7cdf
Melvin Majano <melvinmajano16@gmail.com>
2026-08-19 10:19:29 -0600

    refactor:se cambio los tipos de datos que retornan las funciones (findById,all,create,paginate) de mixed a Model, Collection, Model y LengthAwarePaginator respectivamente

--- api/src/Infrastructure/Contracts/RepositoryInterface.php
+++ api/src/Infrastructure/Contracts/RepositoryInterface.php
@@ -2,17 +2,21 @@
 
 namespace Infrastructure\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 interface RepositoryInterface
 {
-    public function findById(int | string $id):mixed;
+    public function findById(int | string $id):?Model;
 
-    public function all(array $columns=['*']):mixed;
+    public function all(array $columns=['*']):Collection;
 
-    public function create(array $data):mixed;
+    public function create(array $data):Model;
 
     public function uptade(int | string $id,array $data):bool;
 
     public function delete(int | string $id):bool;
     
-    public function paginate(int $perPage=10, $columns=['*']):mixed;
+    public function paginate(int $perPage=10, $columns=['*']):LengthAwarePaginator;
 }
