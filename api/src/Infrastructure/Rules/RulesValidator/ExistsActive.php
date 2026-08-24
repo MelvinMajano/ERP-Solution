d510c72858c3090743d367a9d3c23b7790b946c2
Melvin Majano <melvinmajano16@gmail.com>
2026-08-24 13:43:21 -0600

    feat: se creo la regla de validacion ExistsActive que se encarga de validar que los datos debe existir en el modelo de base de datos, en el campo especifico y debe estar activo.

---
+++ api/src/Infrastructure/Rules/RulesValidator/ExistsActive.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Infrastructure\Rules\RulesValidator;
+
+use Rakit\Validation\Rule;
+
+/**
+ * Validacion el valor debe existir en el modelo de base de datos, en el campo especifico y debe estar activo.
+ */
+class ExistsActive extends Rule
+{
+    public const RULE = 'exists_active';
+    protected $message = ":attribute se encuentra en estado inactivo.";
+    protected $fillableParams = ['modelClass', 'column'];
+
+
+    public function check(
+        mixed $value
+        ): bool
+    {
+
+        // Validar que se hayan proporcionado los parámetros necesarios
+        $this->requireParameters(['modelClass', 'column']);
+        /** @var string $modelClass nombre de clase de tipo `Illuminate\Database\Eloquent\Model`*/
+        $modelClass = $this->parameter('modelClass');
+        /** @var string $column columna en la que se desea buscar el valor*/
+        $column = $this->parameter('column');
+
+        if (!class_exists($modelClass)) {
+            return false;
+        }
+
+        try {
+            return $modelClass::where($column, $value)
+                ->where('status', GeneralStatus::ACTIVE)
+                ->exists();
+        } catch (\Throwable $e) {
+            return false;
+        }
+    }
+}
