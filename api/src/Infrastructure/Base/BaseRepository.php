1ce8df5e72e28b0121889f72228f9eb409dda9a5
Melvin Majano <melvinmajano16@gmail.com>
2026-08-26 18:35:38 -0600

    refactor: se cambiaron algunas cosas de BaseRepository, se agrego applyDynamicFilters, se cambio la logica de all, y se elimino el metodo de paginacion y se agrego el metodo de setStatus que es el que cambia el estado de activo a inactivo en un registro

--- api/src/Infrastructure/Base/BaseRepository.php
+++ api/src/Infrastructure/Base/BaseRepository.php
@@ -20,6 +20,16 @@ abstract class BaseRepository implements RepositoryInterface{
     protected Model $model;
 
     /**
+     * @var array<int, string>
+     */
+    protected array $sortableColumns = ['id'];
+
+    /**
+     * @var array<int, string>
+     */
+    protected array $likeColumns = [];
+
+    /**
      * @param TModel $model
      */
     public function __construct(Model $model)
@@ -50,15 +60,73 @@ abstract class BaseRepository implements RepositoryInterface{
     }
 
     /**
-     * Obtiene todos los registros.
-     *
-     * @param array<int, string> $columns si no se especifica los campos, la cosulta 
-     * traera todos los campos que tenga la tabla en la base de datos
-     * @return Collection<int, TModel> 
+     * Aplica los filtros dinámicos recibidos en $params['filters'].
      */
-    public function all(array $columns=['*']):Collection
+    protected function applyDynamicFilters(Builder $query, array $filters): Builder
     {
-        return $this->query()->get($columns);
+        foreach ($filters as $field => $value) {
+            /**
+             * Valida si los valores de filters son null o vacio se hace un
+             * continue lo representaria que no se apliquen dichos filtros a la query
+             * y esta se retorna sin agregarle nada.
+             */
+            if ($value === null || $value === '') {
+                continue;
+            }
+
+            /**
+             * Si se llega aqui significa de que si hau filtros, y se tiene que deterniminar si son
+             * filtros parciales o exactos, para ello se accede con polimorfismo a la propiedad sobreescrita 
+             * @likeColumns en la clase hija o que herde de BaseRepository
+             */
+            if (in_array($field, $this->likeColumns, true)) {
+                $query->where($field, 'LIKE', "%{$value}%");
+            //Si dicho filtro no se encuentra en likeColumns se considera una busqueda exacta
+            } else {
+                $query->where($field, '=', $value);
+            }
+        }
+
+        return $query;
+    }
+
+    /**
+     * Obtiene todos los registros paginados y ordenados 
+     *
+     * @param array<int, string> $params Parámetros de consulta (paginación, ordenamiento y filtros) 
+     * @return LengthAwarePaginator<TModel> Objeto con la data paginada y metadatos
+     */
+    public function all(array $params = []):LengthAwarePaginator
+    {
+        //Se obtien los datos de paginacion
+        $page = (int) ($params['page'] ?? 1);
+        $pageSize = (int) ($params['pageSize'] ?? 10);
+
+        //los datos de orden y direccion de la misma
+        $rawSortBy = $params['sortBy'] ?? null;
+        $rawSortDir = $params['sortDir'] ?? null;
+
+        //se valida si los paramatros por los que se quiere ordenar son validos estan en sortableColumns
+        $sortBy = in_array($rawSortBy, $this->sortableColumns, true) ? $rawSortBy : 'id';
+        $sortDir = strtolower((string) $rawSortDir) === 'desc' ? 'desc' : 'asc';
+
+        //Crea una nueva instancia de la query
+        $query = $this->query();
+
+        //Ejecuta en applyDynamicFilters que es el que tiene la logiaca de los filtros 
+        if (isset($params['filters']) && is_array($params['filters'])) {
+            $this->applyDynamicFilters($query, $params['filters']);
+        }
+
+        //Ejecuta la consulta en la BD y retorna los datos paginados
+        return $query
+            ->orderBy($sortBy, $sortDir)
+            ->paginate(
+                perPage: $pageSize,
+                columns: ['*'],
+                pageName: 'page',
+                page: $page
+            );
     }
 
     /**
@@ -80,7 +148,7 @@ abstract class BaseRepository implements RepositoryInterface{
      * que retorna el modelo cuando encuentra una considencia 
      * @return bool
      */
-    public function uptade(int | string $id,array $data):bool{
+    public function update(int | string $id,array $data):bool{
         $record = $this->findById($id);
         if(!$record){
             return false;
@@ -105,16 +173,16 @@ abstract class BaseRepository implements RepositoryInterface{
     }
 
     /**
-     * Obtiene los resultados paginados.
-     *
-     * @param int $perPage indica la cantidad de registros que se necesitan, 
-     * si no se envia una cantidad desde el front por defecto se enviaran 10 registros.
-     * @param array<int, string> $columns indica los campos que se necesitan 
-     * @return LengthAwarePaginator<TModel> retorna la data pagina + la informacion de la paginiacion
-     * total de registros, la pagina actual, la ultima pagina disponible, etc.
+     * Cambia el estado de un registro por su ID.
+     * 
+     * @param int|string $id, bool $isActive
+     * @var $record lo mismo que en el update y el delete, se optiene el modelo y
+     * Se actualiza el estado de ese registro 
+     * @return bool
      */
-    public function paginate(int $perPage=10, $columns=['*']):LengthAwarePaginator{
-        return $this->query()->paginate($perPage,$columns);
+   public function setStatus(int|string $id, bool $isActive): bool
+    {
+        $record = $this->findById($id);
+        return $record ? (bool) $record->update(['is_active' => $isActive]) : false;
     }
-
 }
