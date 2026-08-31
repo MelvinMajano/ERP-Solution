93f5163925e8cb390b63485bc8cdde9572cb8587
Melvin Majano <melvinmajano16@gmail.com>
2026-08-31 15:05:56 -0600

    feat: Se agrego el getByIdProductController que es el encargado de recibir la request del client y devolver la data formateada en json del producto obtenido por id

--- api/src/Modules/Inventory/Controllers/ProductController.php
+++ api/src/Modules/Inventory/Controllers/ProductController.php
@@ -2,3 +2,35 @@
 
 namespace Modules\Inventory\Controllers;
 
+use Infrastrucure\Base\BaseController;
+use Modules\Inventory\DTOs\ProductsDtos\GetProductByIdDTO;
+use Modules\Inventory\Services\ProductService;
+use Modules\Inventory\Transformers\ProductTransformer;
+use Modules\Inventory\Validators\ProductValidator;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ProductController extends BaseController
+{
+    public function __construct(
+        private readonly ProductService $productService,
+        private readonly ProductTransformer $productTransformer
+    )
+    {}
+
+    public function getById(Request $request, Response $response, array $args):Response
+    {
+        $id = (int) $args['id'];
+
+        $validatedData = ProductValidator::getByIdValidation($id);
+        $dto = GetProductByIdDTO::fromValidatedData($validatedData);
+
+        $product = $this->productService->getById($dto);
+
+        return $this->jsonResponse(
+            response:$response,
+            data: $this->productTransformer->transform($product),
+            message: 'Producto obtenido con exito',
+        );
+    }
+}
