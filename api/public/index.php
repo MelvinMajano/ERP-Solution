4e9b1084b346a8ec190dbb786aaa042db2683c25
Melvin Majano <melvinmajano16@gmail.com>
2026-09-03 16:41:39 -0600

    refactor: Se modifico el index agregando para poder cargar las regalse de configuracion de dependencias de interfaces

--- api/public/index.php
+++ api/public/index.php
@@ -1,21 +1,19 @@
 <?php
 
-use DI\Container;
+use DI\ContainerBuilder;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
-use Infrastructure\Exceptions\Handler;
 
 require __DIR__ . "/../vendor/autoload.php";
 
 // variables de entorno
 Dotenv::createImmutable(__DIR__ . "/../")->load();
 
-// Crea contenedor e inyecta el Handler
-$container = new Container();
-$container->set(Handler::class ,function(){
-    return new Handler();
-});
-
+//Configura e instancia el contenedor con ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+//Carga las reglas de inyeccion de dependencias
+$containerBuilder->addDefinitions(__DIR__ . '/../config/dependencies.php');
+$container = $containerBuilder->build();
 //configuracion de al app de slim
 AppFactory::setContainer($container);
 $app = AppFactory::create();
@@ -28,4 +26,5 @@ $app->setBasePath($_ENV['API_BASE_PATH']);
 //Carga la configuracion de las rutas 
 (require __DIR__ . '/../config/routes.php')($app);
 
+//Ejecuta la aplicacion
 $app->run();
