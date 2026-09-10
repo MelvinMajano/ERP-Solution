fac1d6cbc397ba03e040825af71110602c9ad982
Melvin Majano <melvinmajano16@gmail.com>
2026-09-10 10:21:16 -0600

    feat: se creo el modelo de user

---
+++ api/src/Domain/Entities/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Domain\Entities;
+
+
+use Domain\Exceptions\DomainException;
+use Illuminate\Database\Eloquent\Model;
+use Infrastructure\Traits\BelongsToTenant;
+
+class User extends Model
+{
+
+    use BelongsToTenant;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'tenant_id',
+        'rol_id',
+        'first_names',
+        'last_names',
+        'username',
+        'email',
+        'password',
+        'is_active',
+        'created_by',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    protected $attributes = [
+        'is_active' => true,
+    ];
+}
