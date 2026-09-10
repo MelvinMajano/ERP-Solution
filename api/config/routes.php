7eecad4c7b3535bdc5cde88237101079845a28f0
Melvin Majano <melvinmajano16@gmail.com>
2026-09-10 15:48:38 -0600

    feat: se agrego el endpoint del registro del tenant al archivo de rutas

--- api/config/routes.php
+++ api/config/routes.php
@@ -1,10 +1,16 @@
 <?php
 
+use Modules\Core\Controllers\OnboardingController;
 use Modules\Inventory\Controllers\ProductController;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app){
+    // Módulo de Autenticación y Onboarding
+    $app->group('/auth', function (RouteCollectorProxy $auth) {
+        // Ruta pública para registro inicial de Tenant
+        $auth->post('/register-tenant', OnboardingController::class);
+    });
     //Modulo de inventario
     $app->group('/products', function(RouteCollectorProxy $products){
         $products->get('',[ProductController::class, 'get']);
