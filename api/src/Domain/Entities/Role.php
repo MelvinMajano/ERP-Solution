199e141bafd1db99e0ff9c755f2766fd7b0b9f58
Melvin Majano <melvinmajano16@gmail.com>
2026-09-10 10:09:01 -0600

    feat: Se creo el modelo del role

---
+++ api/src/Domain/Entities/Role.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Domain\Entities;
+
+use Domain\Exceptions\DomainException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Infrastructure\Traits\BelongsToTenant;
+
+class Role extends Model
+{
+    use BelongsToTenant;
+
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'tenant_id',
+        'name',
+        'description',
+        'created_by',
+    ];
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Permission::class,
+            'rol_permissions',
+            'rol_id',
+            'permission_id'
+        );
+    }
+}
