c24ed053c080963cf09776b9b3af6c83e83d3e92
Melvin Majano <melvinmajano16@gmail.com>
2026-09-19 14:12:13 -0600

    feat: se creo el dto para abrir la caja

---
+++ api/src/Modules/Cash/DTOs/OpenCashBatchDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Cash\DTOs\CashBatchDtos;
+
+readonly class OpenCashBatchDTO
+{
+    public function __construct(
+        public int $cashierUserId,
+        public float $openingBalance = 0.00,
+        public ?int $createdBy = null
+    ) {}
+
+    public static function fromValidatedData(array $validatedData): self
+    {
+        return new self(
+            cashierUserId: (int) $validatedData['cashier_user_id'],
+            openingBalance: (float) ($validatedData['opening_balance'] ?? 0.00),
+            createdBy: isset($validatedData['created_by']) ? (int) $validatedData['created_by'] : null
+        );
+    }
+
+    public function toArray(): array
+    {
+        return array_filter([
+            'cashier_user_id' => $this->cashierUserId,
+            'opening_balance' => $this->openingBalance,
+            'created_by'      => $this->createdBy,
+        ], static fn($val) => $val !== null);
+    }
+}
