e166435b4a7bf6054bd3c6b8c1b7798fb6485f63
Melvin Majano <melvinmajano16@gmail.com>
2026-09-21 15:14:17 -0600

    feat: se agrego la configuracion de dependencias al archivo de dependencia

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -10,6 +10,8 @@ use Modules\Core\Repositories\TenantRepository;
 use Modules\Core\Repositories\UserRepository;
 use Modules\Core\Repositories\RoleRepository;
 use Modules\Core\Repositories\PermissionRepository;
+use Domain\Contracts\MovementReasonRepositoryInterface;
+use Modules\Inventory\Repositories\MovementReasonRepository;
 use function DI\autowire;
 
 /**
@@ -29,4 +31,5 @@ return[
     UserRepositoryInterface::class => DI\autowire(UserRepository::class),
     RoleRepositoryInterface::class => DI\autowire(RoleRepository::class),
     PermissionRepositoryInterface::class => DI\autowire(PermissionRepository::class),
+    MovementReasonRepositoryInterface::class => DI\autowire(MovementReasonRepository::class),
 ];
